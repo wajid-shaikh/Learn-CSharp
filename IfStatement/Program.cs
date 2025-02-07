490931c9ac0aeac560e90a2ca19a7298644c3bbd
wajid-shaikh <wajid777shaikh@gmail.com>
2025-02-08 00:36:36 +0530

    Initial commit with C# project exmaple ( IfStatement )

---
+++ IfStatement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class IfExample
+{
+    public static void Main(string[] args)
+    {
+        int num = 0;
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("It is an Even number");
+        }
+    }
+}
