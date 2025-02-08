dd1095b561c0c90d627237d18432b60b8197ae74
wajid-shaikh <wajid777shaikh@gmail.com>
2025-02-08 17:07:38 +0530

    Call by value and Call by reference examples

---
+++ MoreOnFunctions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MoreOnFunctions
+{
+    class MoreOnFunctions
+    {
+        public static void Main(string[] args)
+        {
+            int val = 20;
+
+            // Call by value example
+            Console.WriteLine($"Value before calling show function {val}");
+
+            CallByValue cbv = new CallByValue();
+            cbv.Show(val);
+
+            Console.WriteLine($"Value after calling show function {val}");
+
+            // Call by reference example
+            Console.WriteLine($"Value before calling show function {val}");
+
+            CallByReference cbr = new CallByReference();
+            cbr.Show(ref val);
+
+            Console.WriteLine($"Value after calling show function {val}");
+        }
+    }
+
+    class CallByValue
+    {
+        public void Show(int val)
+        {
+            val = val + 10;
+            Console.WriteLine($"Value inside show function {val}");
+        }
+    }
+
+    class CallByReference
+    {
+        public void Show(ref int val)
+        {
+            val = val + 10;
+            Console.WriteLine($"Value inside show function {val}");
+        }
+    }
+}
