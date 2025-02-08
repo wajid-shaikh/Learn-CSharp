1cf37d191c94456ff59a2ccbf78eac6279325745
wajid-shaikh <wajid777shaikh@gmail.com>
2025-02-08 21:53:19 +0530

    ParamsExample

---
+++ Params/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+class ParamsExample
+{
+
+    public void UserDetails(params object[] user)
+    {
+        for(int i=0; i<user.Length; i++)
+        {
+            Console.WriteLine(user[i]);
+        }
+    }
+    public static void Main(string[] args)
+    {
+        ParamsExample pe = new ParamsExample();
+        pe.UserDetails("Ramakrishnan Ayyer", "Ramesh", 101, 20.5, "Peter", "A");
+    }
+}
