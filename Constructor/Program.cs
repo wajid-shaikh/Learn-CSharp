e5d2c63417d44eea010346922beebe703ec7f54b
wajid-shaikh <wajid777shaikh@gmail.com>
2025-02-08 23:24:58 +0530

    All about Constructor

---
+++ Constructor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AboutConstructor
+{
+    class Employee
+    {
+        public int Id;
+        public string Name;
+        public string Department;
+        public Employee(int id, string name, string department)
+        {
+            Id = id;
+            Name = name;
+            Department = department;
+        }
+        public void EmployeeInfo() 
+        {
+            Console.WriteLine($"Employee Info.\nId: {Id}\nName: {Name}\nDepartment: {Department}");
+        }
+        public static void Main(string[] args)
+        {
+            Employee emp = new Employee(786,"Joe","IT");
+            emp.EmployeeInfo();
+        }
+    }
+
+}
