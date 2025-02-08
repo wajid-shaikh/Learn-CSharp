dc300bed239101f966bb259e392be7cf2a3b1ce2
wajid-shaikh <wajid777shaikh@gmail.com>
2025-02-08 20:29:58 +0530

    Example on Types of Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Arrays
+{
+    class TypesOfArrays
+    {
+        public static void Main(string[] args)
+        {
+            SingleDimensional sd = new SingleDimensional();
+            for(int i=0; i<sd.numbers.Length; i++)
+            {
+                int val = sd.numbers[i];
+                Console.WriteLine(val);
+            }
+        }
+    }
+
+    class SingleDimensional
+    {
+        public int[] numbers = new int[] { 1, 2, 3, 4, 5, 6 };
+    }
+}
