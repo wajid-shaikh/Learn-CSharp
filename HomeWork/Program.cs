dfaac3264c7e5258b84666d078c9ec27667cc468
wajid-shaikh <wajid777shaikh@gmail.com>
2025-02-15 21:01:34 +0530

    Example about This keyword

---
+++ HomeWork/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Homework
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Enter the count of numbers : ");
+        string count = Console.ReadLine();
+        Console.WriteLine(count);
+    }
+}
---
+++ ThisKeywordexample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class ThisKeyword
+{
+    public int Id;
+    public string Name;
+    public float Score;
+    public ThisKeyword (int id, string name, float score)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Score = score;
+    }
+    public string DisplayDetails()
+    {
+        return $"Name: {Name} \nExam id: {Id} \nScore: {Score}";
+    }
+
+    public static void Main(string[] args)
+    {
+        ThisKeyword tk = new ThisKeyword(786, "Jason", 98.34f);
+
+        Console.WriteLine(tk.DisplayDetails());
+    }
+}
