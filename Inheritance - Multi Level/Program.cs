100cea00804f744750e64909f3fcb0880f908cdc
wajid-shaikh <wajid777shaikh@gmail.com>
2025-02-17 00:07:49 +0530

    Example on Multi Level Inheritance

---
+++ Inheritance - Multi Level/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Exam1
+{
+    public void FirstExam()
+    {
+        Console.WriteLine("Passed in first test");
+    }
+}
+
+class Exam2 : Exam1
+{
+    public void SecondExam()
+    {
+        Console.WriteLine("Passed in second test");
+    }
+}
+
+class SSCCGL : Exam2
+{
+    public void Result()
+    {
+        Console.WriteLine("Congrats, You are selected!!!");
+    }
+}
+
+class MultiLevel
+{
+    public static void Main(string[] args)
+    {
+        SSCCGL sc = new SSCCGL();
+        sc.FirstExam();
+        sc.SecondExam();
+        sc.Result();
+    }
+}
