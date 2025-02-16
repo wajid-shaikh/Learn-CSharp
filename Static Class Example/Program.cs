8acaf6b3cc03aa89928319b0cf2678ede0e8dd60
wajid-shaikh <wajid777shaikh@gmail.com>
2025-02-16 22:26:46 +0530

    Example on Static Class

---
+++ Static Class Example/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+static class StaticClass
+{
+    public static int id = 45;
+    public static string name = "Jony";
+    public static float marks = 45.4f;
+
+    public static void Display()
+    {
+        Console.WriteLine($"id: {id}\nname: {name}\nmarks: {marks}");
+    }
+}
+
+class StaticClassExample
+{
+    public static void Main( string[] args)
+    {
+        StaticClass.name = "Jessica";
+        StaticClass.Display();
+    }
+}
