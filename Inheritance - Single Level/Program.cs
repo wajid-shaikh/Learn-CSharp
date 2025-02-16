7433500c9c52774e78caf28d45fadc255def3c65
wajid-shaikh <wajid777shaikh@gmail.com>
2025-02-16 23:45:12 +0530

    Example on Single Level Inheritance

---
+++ Inheritance - Single Level/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class ParentClass
+{
+    public string firstName = "Jason";
+}
+
+class ChildClass : ParentClass
+{
+    public string lastName = "Mamoa";
+}
+
+class SlingleLevelInheritance
+{
+    public static void Main(string[] args)
+    {
+        ChildClass c = new ChildClass();
+        string fullName = c.firstName +" "+ c.lastName;
+        Console.WriteLine(fullName);
+    }
+}
