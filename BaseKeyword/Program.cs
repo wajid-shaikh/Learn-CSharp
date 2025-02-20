3961eb856a3b50c548ec62839828dc8687a720f3
wajid-shaikh <wajid777shaikh@gmail.com>
2025-02-20 23:16:17 +0530

    Base Keyword Example

---
+++ BaseKeyword/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void Eat()
+    {
+        Console.WriteLine("Drinks Milk");
+    }
+}
+
+class Cat : Animal
+{
+    public override void Eat()
+    {
+        Console.WriteLine("Eats Fish...");
+    }
+}
+class SnowBall : Cat
+{
+    public override void Eat()
+    {
+        base.Eat();
+        Console.WriteLine("Eats Mice");
+    }
+}
+
+class BaseKeyword
+{
+    public static void Main(string[] args)
+    {
+        SnowBall sb = new SnowBall();
+        sb.Eat();
+    }
+}
