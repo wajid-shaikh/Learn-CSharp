229d12033817e2f136ddcdf349d83c678d934061
wajid-shaikh <wajid777shaikh@gmail.com>
2025-02-20 22:35:59 +0530

    Static Class Method Overloading Example

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class MethodOverloading
+{
+    public int add(int a, int b)
+    {
+        return a + b;
+    }
+    public int add(int a, int b, int c) 
+    {  
+        return a + b + c; 
+    }
+
+    public static void Main(string[] args)
+    {
+        MethodOverloading mo = new MethodOverloading();
+        Console.WriteLine(mo.add(1, 2));
+        Console.WriteLine(mo.add(1, 2, 3));
+    }
+}
