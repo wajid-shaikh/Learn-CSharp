6d3cfd08a17ee17d7964841795c065d17da23184
wajid-shaikh <wajid777shaikh@gmail.com>
2025-02-20 22:54:19 +0530

    Method Overriding Example

---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,32 @@
+﻿class Animal
+{
+    public virtual void Eats()
+    {
+        Console.WriteLine("Eating grass");
+    }
+}
+
+class Dog : Animal
+{
+    public override void Eats()
+    {
+        Console.WriteLine("Eating Pedigree...");
+    }
+}
+
+class BullDog : Dog
+{
+    public override void Eats() 
+    { 
+        Console.WriteLine("Eating bones........");
+    }
+}
+
+class MethodOverridden
+{
+    public static void Main(string[] args)
+    {
+        BullDog bd = new BullDog();
+        bd.Eats();
+    }
+}
