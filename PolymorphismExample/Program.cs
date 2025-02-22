90c826160d2e33c1be0c80a7d688f7241747d9c0
wajid-shaikh <wajid777shaikh@gmail.com>
2025-02-22 23:14:44 +0530

    Runtime Polymorphism Example

---
+++ PolymorphismExample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Shape
+{
+    public virtual void Draws()
+    {
+        Console.WriteLine("Drawing...");
+    }
+}
+
+class Circle : Shape
+{
+    public override void Draws()
+    {
+        Console.WriteLine("Drawing circle...");
+    }
+}
+
+class Triangle : Shape
+{
+    public override void Draws()
+    {
+        Console.WriteLine("Drawing triangle...");
+    }
+}
+
+class Rectangle : Shape
+{
+    public override void Draws()
+    {
+        Console.WriteLine("Drawing rectangle...");
+    }
+}
+
+class RuntimePolymorphism
+{
+    public static void Main(string[] args)
+    {
+        Shape a;
+        a = new Shape();
+        a.Draws();
+        a = new Circle();
+        a.Draws();
+        a = new Triangle();
+        a.Draws();
+        a = new Rectangle();
+        a.Draws();
+    }
+}
