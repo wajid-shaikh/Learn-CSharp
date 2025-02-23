3e98be73a30068347e80420bd7ff03f7a9060b13
wajid-shaikh <wajid777shaikh@gmail.com>
2025-02-23 19:13:54 +0530

    Abstraction Example

---
+++ Abstraction Example/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+abstract class Animal
+{
+    public abstract void MakeSound();
+}
+
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Barking...");
+    }
+}
+
+class Cats : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meowing...");
+    }
+}
+
+class AbstractClassExample
+{
+    public static void Main(string[] args)
+    {
+        Animal a;
+        a = new Dog();
+        a.MakeSound();
+        a = new Cats();
+        a.MakeSound();
+    }
+}
