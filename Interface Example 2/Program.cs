9b0e536f784e0bc4e832cd48101b0f99973408ca
wajid-shaikh <wajid777shaikh@gmail.com>
2025-02-23 22:07:03 +0530

    Interface Example 2 - Supports multiple inheritance

---
+++ Interface Example 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+interface ISamsung
+{
+    void CameraPixel();
+}
+
+interface IApple
+{
+    void CameraPixel();
+}
+
+class Phone : ISamsung, IApple
+{
+    void ISamsung.CameraPixel()
+    {
+        Console.WriteLine("Samsung has max of 120 mega pixels ...");
+    }
+
+    void IApple.CameraPixel()
+    {
+        Console.WriteLine("Apple has max of 30 mega pixels ...");
+    }
+}
+
+class InterfaceExample
+{
+    public static void Main(string[] args)
+    {
+        // 1st way
+        //Phone phone = new Phone();
+        //((ISamsung)phone).CameraPixel();
+        //((IApple)phone).CameraPixel();
+
+        // 2nd way
+        IApple a = new Phone();
+        a.CameraPixel();
+
+        ISamsung s = new Phone();
+        s.CameraPixel();
+    }
+}
