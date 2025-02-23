9e68f3de734ab1de51cb21e7024bbe736bb5bcab
wajid-shaikh <wajid777shaikh@gmail.com>
2025-02-23 20:49:48 +0530

    Interface Example 1

---
+++ Interface Example 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+interface IAnimal
+{
+    void MakeSound();
+}
+
+class Dog : IAnimal
+{
+    public void MakeSound()
+    {
+        Console.WriteLine("Barks...");
+    }
+}
+class Cat : IAnimal
+{
+    public void MakeSound()
+    {
+        Console.WriteLine("Meows...");
+    }
+}
+
+class InterfaceExample
+{
+    public static void Main(string[] args)
+    {
+        IAnimal dog = new Dog();
+        dog.MakeSound();
+        IAnimal cat = new Cat();
+        cat.MakeSound();
+    }
+}
